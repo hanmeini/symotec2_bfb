e8cecbb5352bcf757632ebe91e31715ce8bb4aa9
hanmeini <ahmadraihan607@gmail.com>
2026-07-08 10:08:51 +0700

    chore: remove obsolete database migration and utility scripts

--- count_b.php
+++
@@ -1,4 +0,0 @@
-<?php
-require 'config1.php';
-$res = $conn->query("SELECT COUNT(*) as c FROM b");
-echo $res->fetch_assoc()['c'];
--- describe.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'config1.php';
-$r = $conn->query('DESCRIBE penjualanHO1');
-while($row = $r->fetch_assoc()) echo $row['Field'] . ' ';
-echo "\n";
